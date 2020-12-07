a6dad78ab99d62445c637167e8d808d166651e8a
“[Nicol <[nicol.saha@gmail.com]>
2020-12-07 19:04:53 +0100

    Add posts and show alert, add removing post functionality

--- crud/src/app.js
+++ crud/src/app.js
@@ -4,9 +4,55 @@ import { ui } from './ui';
 // GET POSTS ON DOM LOAD
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// LISTEN FOR ADD POST
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// LISTEN FOR DELETE
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// GET POSTS
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
+}
+
+// SUBMIT POSTS
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body,
+  };
+}
+
+//DELETE POST
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  // CREATE POST
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post Added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch((err) => console.log(err));
 }
